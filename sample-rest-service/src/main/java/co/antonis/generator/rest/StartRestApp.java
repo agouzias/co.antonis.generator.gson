53ad7e94061a40576dee21c7d5ce201b14494d01
Antonis Gouzias (home) <agouzias@gmail.com>
2023-02-23 18:06:52 +0200

    Adding sample-gwt-app (renaming)

--- sample-rest-service/src/main/java/co/antonis/generator/rest/StartRestApp.java
+++ sample-rest-service/src/main/java/co/antonis/generator/rest/StartRestApp.java
@@ -2,7 +2,6 @@ package co.antonis.generator.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
